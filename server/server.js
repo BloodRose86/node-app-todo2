c8cb9ae3bf4650b05f214c32057cc0185c88a7e4
BloodRose86 <andre@aj-systems.co.za>
2016-12-21 18:08:06 +0200

    Adding server Express

--- server/server.js
+++ server/server.js
@@ -5,3 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./moduls/todo');
 var {User} = require('./moduls/user');
 
+var app = express();
+
+app.listen(3000, () => {
+    console.log('server on port 3000');
+});
